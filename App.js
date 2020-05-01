1ad5a95c94568b0991ad4f5d9aa543d0acf98dd5
mukulietlucknow <mukul.ietlucknow@gmail.com>
2020-05-01 22:22:25 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , TextInput , Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding : 80}}>
+      <View style={{flexDirection : 'row', justifyContent : 'space-between'}}>
+        <TextInput placeholder="Cource level" style={{width : '80%' , borderBottomColor : 'blueviolet'  , borderBottomWidth : 1}}/>
+        <Button title="ADD"/>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
